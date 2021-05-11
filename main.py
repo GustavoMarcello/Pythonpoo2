092bb52ad404986c8346aef63b01a4359180e1f4
GustavoMarcello <gustavo.g.marcello@gmail.com>
2021-05-11 16:39:56 -0300

    implantando funções list

--- main.py
+++ main.py
@@ -2,11 +2,26 @@ from modelo import *
 
 
 vingadores = Filme('avengers - Infinite war', 2018, 160)
-print(f'{vingadores.nome} - {vingadores.duracao} - Likes: {vingadores.likes}')
+lotr = Filme('o senhor dos aneis - o retorno do rei', 2003, 200)
+got = Serie('Game of thrones', 2013, 7)
+sherlock = Serie('sherlock', 2018, 4)
+
+vingadores.dar_like()
+lotr.dar_like()
+vingadores.dar_like()
+got.dar_like()
+vingadores.dar_like()
+sherlock.dar_like()
+sherlock.dar_like()
 vingadores.dar_like()
 
-got = Serie('Game of thrones', 2013, 7)
-print(f'Nome: {got.nome} - Ano: {got.ano} - Temporadas: {got.temporada} - Likes: {got.likes}')
-got.nome = 'gerra dos tronos'
-got.dar_like()
-print(f'Nome: {got.nome} - Ano: {got.ano} - Temporadas: {got.temporada} - Likes: {got.likes}')
+
+filmes_series = [vingadores, got, lotr, sherlock]
+playlist_fim_de_semana = Playlist('sessão pipoca', filmes_series)
+
+print(f'Tamanho do playlist: {len(playlist_fim_de_semana)}')
+
+for programa in playlist_fim_de_semana:
+    print(programa)
+
+print(f' Tá na lista?: {lotr in playlist_fim_de_semana}')
--- modelo.py
+++ modelo.py
@@ -18,15 +18,30 @@ class Programa:
     @nome.setter
     def nome(self, novo_nome):
         self._nome = novo_nome.title()
+    
+    def __str__(self):
+        return f'{self.nome} - {self.ano} - {self.likes} Likes'
 
 
 class Filme(Programa):
     def __init__(self, nome, ano, duracao):
         super().__init__(nome, ano)
         self.duracao = duracao
+    
+    def __str__(self):
+        return f'{self.nome} - {self.ano} - {self.duracao} mins - {self.likes} Likes'
 
 
 class Serie(Programa):
     def __init__(self, nome, ano, temporada):
         super().__init__(nome, ano)
         self.temporada = temporada
+
+    def __str__(self):
+        return f'{self.nome} - {self.ano} - {self.temporada} temporadas - {self.likes} Likes'
+
+
+class Playlist(list):
+    def __init__(self, nome, programas):
+        self.nome = nome
+        super().__init__(programas)
