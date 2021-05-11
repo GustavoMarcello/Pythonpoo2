86ac68b0b413fd266d1cf74d621e9a18c582824c
GustavoMarcello <gustavo.g.marcello@gmail.com>
2021-05-11 10:25:55 -0300

    Código aprimorado

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from modelo import *
 
+
 vingadores = Filme('avengers - Infinite war', 2018, 160)
-print(f'Nome: {vingadores.nome} - Ano: {vingadores.ano} - Duração: {vingadores.duracao}')
+print(f'Nome: {vingadores.nome} - Ano: {vingadores.ano} - Duração: {vingadores.duracao} - Likes: {vingadores.likes}')
+vingadores.dar_like()
 
 got = Serie('Game of thrones', 2013, 7)
-print(f'Nome: {got.nome} - Ano: {got.ano} - Temporadas: {got.temporada}')
+print(f'Nome: {got.nome} - Ano: {got.ano} - Temporadas: {got.temporada} - Likes: {got.likes}')
+got.dar_like()
--- modelo.py
+++ modelo.py
@@ -1,11 +1,20 @@
 class Filme:
     def __init__(self, nome, ano, duracao):
-        self.nome = nome
+        self.nome = nome.title()
         self.ano = ano
         self.duracao = duracao
+        self.likes = 0
+
+    def dar_like(self):
+        self.likes +=1
 
 class Serie:
-        def __init__(self, nome, ano, temporada):
-            self.nome = nome
-            self.ano = ano
-            self.temporada = temporada
+    def __init__(self, nome, ano, temporada):
+        self.nome = nome.title()
+        self.ano = ano
+        self.temporada = temporada
+        self.likes = 0
+
+
+    def dar_like(self):
+        self.likes +=1
