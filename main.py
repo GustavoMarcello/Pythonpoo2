3a829a7c0cf89db8161ae473adeb817b0cf0d5cc
GustavoMarcello <gustavo.g.marcello@gmail.com>
2021-05-11 12:55:06 -0300

    commit

--- main.py
+++ main.py
@@ -2,9 +2,11 @@ from modelo import *
 
 
 vingadores = Filme('avengers - Infinite war', 2018, 160)
-print(f'Nome: {vingadores.nome} - Ano: {vingadores.ano} - Duração: {vingadores.duracao} - Likes: {vingadores.likes}')
+print(f'{vingadores.nome} - {vingadores.duracao} - Likes: {vingadores.likes}')
 vingadores.dar_like()
 
 got = Serie('Game of thrones', 2013, 7)
 print(f'Nome: {got.nome} - Ano: {got.ano} - Temporadas: {got.temporada} - Likes: {got.likes}')
+got.nome = 'gerra dos tronos'
 got.dar_like()
+print(f'Nome: {got.nome} - Ano: {got.ano} - Temporadas: {got.temporada} - Likes: {got.likes}')
--- modelo.py
+++ modelo.py
@@ -1,20 +1,32 @@
-class Filme:
+class Programa:
+    def __init__(self, nome, ano):
+        self._nome = nome.title()
+        self.ano = ano
+        self._likes = 0
+
+    @property
+    def likes(self):
+        return self._likes
+
+    def dar_like(self):
+        self._likes += 1
+
+    @property
+    def nome(self):
+        return self._nome
+
+    @nome.setter
+    def nome(self, novo_nome):
+        self._nome = novo_nome.title()
+
+
+class Filme(Programa):
     def __init__(self, nome, ano, duracao):
-        self.nome = nome.title()
-        self.ano = ano
+        super().__init__(nome, ano)
         self.duracao = duracao
-        self.likes = 0
 
-    def dar_like(self):
-        self.likes +=1
 
-class Serie:
+class Serie(Programa):
     def __init__(self, nome, ano, temporada):
-        self.nome = nome.title()
-        self.ano = ano
+        super().__init__(nome, ano)
         self.temporada = temporada
-        self.likes = 0
-
-
-    def dar_like(self):
-        self.likes +=1
