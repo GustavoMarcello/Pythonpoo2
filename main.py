899fce875dfcd815d7bc4867d1ee8537d4e48761
GustavoMarcello <gustavo.g.marcello@gmail.com>
2021-05-11 09:51:44 -0300

    Primeiros passos curso python poo2

---
+++ main.py
@@ -0,0 +1,7 @@
+from modelo import *
+
+vingadores = Filme('avengers - Infinite war', 2018, 160)
+print(f'Nome: {vingadores.nome} - Ano: {vingadores.ano} - Duração: {vingadores.duracao}')
+
+got = Serie('Game of thrones', 2013, 7)
+print(f'Nome: {got.nome} - Ano: {got.ano} - Temporadas: {got.temporada}')
---
+++ modelo.py
@@ -0,0 +1,11 @@
+class Filme:
+    def __init__(self, nome, ano, duracao):
+        self.nome = nome
+        self.ano = ano
+        self.duracao = duracao
+
+class Serie:
+        def __init__(self, nome, ano, temporada):
+            self.nome = nome
+            self.ano = ano
+            self.temporada = temporada
